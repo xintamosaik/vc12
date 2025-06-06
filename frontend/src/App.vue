61ccf8fe0fb3491f8b374da8aa1625fe587119b4
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-06 11:22:54 +0200

    feat: restructure App.vue and update routing; remove HelloWorld component and add TestView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
+        
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/devdocs">Dev Docs</RouterLink>
       </nav>
     </div>
   </header>
@@ -26,16 +25,12 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+
 }
 
 nav a.router-link-exact-active {
@@ -75,11 +70,11 @@ nav a:first-of-type {
 
   nav {
     text-align: left;
-    margin-left: -1rem;
+
     font-size: 1rem;
 
     padding: 1rem 0;
-    margin-top: 1rem;
+  
   }
 }
 </style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,8 +5,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/devdocs',
+      name: 'devdocs',
       component: HomeView,
     },
     {
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+       {
+      path: '/',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/TestView.vue'),
     },
   ],
 })
