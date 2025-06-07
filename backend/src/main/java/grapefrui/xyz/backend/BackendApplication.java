83fea3caae5251278fde520e1f6f1a51a41e025e
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-07 13:16:19 +0200

    refactor: reorganize imports and improve code formatting in BackendApplication.java

--- backend/src/main/java/grapefrui/xyz/backend/BackendApplication.java
+++ backend/src/main/java/grapefrui/xyz/backend/BackendApplication.java
@@ -1,18 +1,41 @@
 package grapefrui.xyz.backend;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
 public class BackendApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(BackendApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hi %s!", name);
+  }
+
+  @PostMapping("/intel/new")
+  public String postMethodName(@RequestBody String entity) {
+    System.out.println("Received entity: " + entity);
+    return entity;
+  }
+
+  @Configuration
+  public static class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/**")
+              .addResourceLocations("file:frontend/dist/");
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hi %s!", name);
-    }
+  }
+
 }
